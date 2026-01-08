58a2d48591f931ec3fdf9f19a22024796de3c2d1
diegociarafoni <diego.ciarafoni@gmail.com>
2026-01-08 23:15:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'board-turn-helper',
+  webDir: 'www'
+};
+
+export default config;
